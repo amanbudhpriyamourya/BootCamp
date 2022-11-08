81273b249804a162574a3fa355a7c784d2c76c7b
Aman Budhpriya Mourya <73774081+amanbudhpriyamourya@users.noreply.github.com>
2022-11-08 12:41:14 +0530

    Add files via upload

---
+++ Conditon and ternary.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main (){
+    int rating = 5;
+    printf("Your rating feedback is %s\n" , rating>4 ? "true block" : "false block");
+    
+    return 0;
+}
